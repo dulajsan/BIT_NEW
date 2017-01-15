5865fc8c571897d6e35a29054034d67abe252c3a
dulajsan <dulaj.san@gmail.com>
2017-01-15 15:52:05 +0530

    director middleware

---
+++ app/Http/Middleware/Director.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Director
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+      if(Auth::check() && Auth::user()->role==5){
+         return $next($request);
+
+     }
+
+     return redirect('login');
+    }
+}
